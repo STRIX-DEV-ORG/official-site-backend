eedf471314f54b01017dbcc402418d440535dc6f
maneroto <maneroto@hotmail.com>
2023-04-19 17:41:44 -0600

    Modify url for production server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.example.com',
+  url: 'https://admin.strixdev.net',
   app: {
     keys: env.array('APP_KEYS'),
   },
